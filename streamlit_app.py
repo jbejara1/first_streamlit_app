209036f1d95e9a74f4dddee1e021f788015f0326
Valentina Bejarano <jbejara1@binghamton.edu>
2022-04-27 22:42:48 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 
 #create dropdown list
 my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+fruitsToShow = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avacado','Strawberries'])
 
 #display table
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruitsToShow)
