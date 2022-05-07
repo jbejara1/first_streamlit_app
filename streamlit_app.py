6e7dd54db6ebf385d7d9c2599ffd075cb9af06dc
Valentina Bejarano <jbejara1@binghamton.edu>
2022-05-07 19:23:41 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,13 +41,51 @@ streamlit.dataframe(fruityviceNormalized)
 import snowflake.connector
 import snowflake.connector
 
-myCnx = snowflake.connector(**streamlit.secrets["Snowflake"])
+import streamlit
 
-myCur = myCnx.cursor()
+streamlit.title('My Mom\'s New Healthy Diner')
 
-myCur.execute("SELECT CURRENT_USER(), current_account(), current_region()")
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 
-myDataRow = myCur.fetchone()
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-streamlit.text("Hi from Snowflake")
-streamlit.text(myDataRow)
+
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+
+#create dropdown list
+my_fruit_list = my_fruit_list.set_index('Fruit')
+fruitsSelected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
+fruitsToShow = my_fruit_list.loc[fruitsSelected]
+
+#display table
+streamlit.dataframe(fruitsToShow)
+
+streamlit.header('Fruityvice Fruit Advise')
+
+fruitChoice = streamlit.text_input('What fruit would you like info about?','Kiwi')
+streamlit.write('The user entered',fruitChoice)
+
+import requests
+fruitJuiceResponse = requests.get("https://fruityvice.com/api/fruit/"+fruitChoice)
+#streamlit.text(fruitJuiceResponse.json()) #writes to the screen
+
+#normalize - make table 
+fruityviceNormalized = pandas.json_normalize(fruitJuiceResponse.json())
+#output to screen
+streamlit.dataframe(fruityviceNormalized)
+
+import snowflake.connector
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
