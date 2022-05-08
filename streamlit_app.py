b74159f9a2b9280fa2a8efba2eb28698223a9d2d
Valentina Bejarano <jbejara1@binghamton.edu>
2022-05-07 20:01:10 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ except URLError as e:
   
 streamlit.header("The fruit list contains:")
 def getFruitList():
-  with my_cur = my_cnx.cursor() as my_cur:
+  with my_cnx.cursor() as my_cur:
     my_cur.execute("SELECT * from fruit_load_list")
     return my_cur.fetchall()
   
